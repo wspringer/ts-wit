5749e3aad0a81d8bafb2abe3c1e69c9f682b7615
Wilfred Springer <wilfred@eastpole.nl>
2025-04-15 18:42:06 +0200

    Added a fairly sensible abstraction to grow the model

--- src/model/model.types.ts
+++ src/model/model.types.ts
@@ -1,84 +1,102 @@
-export type Package = {
-  packages: NestedPackage[];
-} & InterfaceContainer;
+/**
+ * A utility type used for resolving extra metadata associated to WIT types.
+ * Used to create data structures that are similar but extended in depth at
+ * various stages of the parsing process. (This prevents use from having to
+ * repeat the same structures over and over again.)
+ */
+type MetaOf<M, T extends string> = M extends {
+  [K in T]: infer U;
+}
+  ? U
+  : {};
 
-export type QualifiedPackage = {
+export type Package<M> = {
+  packages: NestedPackage<M>[];
+} & InterfaceContainer<M>;
+
+export type QualifiedPackage<M> = {
   name: string;
   version: string;
 };
 
-export type AnonymousPackage = {
+export type AnonymousPackage<M> = {
   name?: undefined;
   version?: undefined;
 };
 
-export type RootPackage = Package &
-  (QualifiedPackage | AnonymousPackage) & {
-    packages: NestedPackage[];
+export type RootPackage<M> = Package<M> &
+  (QualifiedPackage<M> | AnonymousPackage<M>) & {
+    packages: NestedPackage<M>[];
   };
 
-export type NestedPackage = Package & {
+export type NestedPackage<M> = Package<M> & {
   name: string;
 };
 
-export type Wit = RootPackage &
-  WorldContainer & {
-    uses: Use[];
+export type Wit<M = {}> = RootPackage<M> &
+  WorldContainer<M> & {
+    uses: Use<M>[];
   };
 
-export type Use = {
+export type Use<M> = {
   path: string;
   alias?: string;
 };
 
-export type WorldContainer = {
-  worlds: World[];
+export type WorldContainer<M> = {
+  worlds: World<M>[];
 };
 
-export type InterfaceContainer = {
-  interfaces: Interface[];
+export type InterfaceContainer<M> = {
+  interfaces: Interface<M>[];
 };
 
-export type World = {
+export type World<M> = {
   name: string;
   exports: {
-    functions: Func[];
-    interfaces: (Interface | InterfaceRef)[];
+    functions: Func<M>[];
+    interfaces: (Interface<M> | InterfaceRef<M>)[];
   };
   imports: {
-    functions: Func[];
-    interfaces: (Interface | InterfaceRef)[];
+    functions: Func<M>[];
+    interfaces: (Interface<M> | InterfaceRef<M>)[];
   };
 };
 
-export type Func = {
+export type Func<M> = {
   name: string;
-  params: Param[];
-  result?: Type;
+  params: Param<M>[];
+  result?: Ty<M>;
 };
 
-export type Param = {
+export type Param<M> = {
   name: string;
-  type: Type;
+  type: Ty<M>;
 };
 
-export type Interface = {
+export type Interface<M> = {
   name: string;
-  functions: Func[];
-  typeDefs: TypeDef[];
-};
+  functions: Func<M>[];
+  typeDefs: TypeDef<M>[];
+} & MetaOf<M, "Interface">;
 
-export type InterfaceRef = {
+export type InterfaceRef<M> = {
   name: string;
+} & MetaOf<M, "InterfaceRef">;
+
+export type TypeDef<M> = (
+  | AliasDef<M>
+  | EnumDef<M>
+  | VariantDef<M>
+  | RecordDef<M>
+) &
+  MetaOf<M, "TypeDef">;
+
+export type TupleType<M> = {
+  items: Ty<M>[];
 };
 
-export type TypeDef = AliasDef | EnumDef | VariantDef | RecordDef;
-
-export type TupleType = {
-  items: Type[];
-};
-
-export type SimpleType =
+export type SimpleType<M> =
   | "s8"
   | "s16"
   | "s32"
@@ -93,69 +111,118 @@ export type SimpleType =
   | "string"
   | "char";
 
-export type RecordDef = {
+export type RecordDef<M> = {
+  kind: "record";
   name: string;
-  fields: RecordField[];
+  fields: RecordField<M>[];
 };
 
-export type RecordField = {
+export type RecordField<M> = {
   name: string;
-  type: Type;
+  type: Ty<M>;
 };
 
-export type EnumDef = {
+export type EnumDef<M> = {
+  kind: "enum";
   name: string;
   cases: string[];
 };
 
-export type OptionType = {
+export type OptionType<M> = {
   kind: "option";
-  type: Type;
+  type: Ty<M>;
 };
 
-export type ListType = {
+export type ListType<M> = {
   kind: "list";
-  type: Type;
+  type: Ty<M>;
 };
 
-export type ResultType = {
+export type ResultType<M> = {
   kind: "result";
-  ok?: Type;
-  error?: Type;
+  ok?: Ty<M>;
+  error?: Ty<M>;
 };
 
-export type VariantDef = {
+export type VariantDef<M> = {
+  kind: "variant";
   name: string;
-  cases: VariantCase[];
+  cases: VariantCase<M>[];
 };
 
-export type VariantCase = {
+export type VariantCase<M> = {
   name: string;
-  type?: Type;
+  type?: Ty<M>;
 };
 
-export type AliasDef = {
+export type AliasDef<M> = {
+  kind: "alias";
   name: string;
-  type: Type;
+  type: Ty<M>;
 };
 
-export type Type =
-  | TypeRef
-  | SimpleType
-  | OptionType
-  | ListType
-  | ResultType
-  | TupleType;
+export type Ty<M> =
+  | TypeRef<M>
+  | SimpleType<M>
+  | OptionType<M>
+  | ListType<M>
+  | ResultType<M>
+  | TupleType<M>;
 
-export type TypeRef = {
+export type TypeRef<M> = {
   name: string;
+} & MetaOf<M, "TypeRef">;
+
+export type Qualified = {
+  fqn: string;
 };
+
+export type Resolved<T> = {
+  resolved: T | null;
+};
+
+/**
+ * A qualified Wit is a Wit with all the types and interfaces qualified with
+ * their FQN. (e.g. `other:foo/bar`)
+ */
+export type QualifiedWit = Wit<{
+  TypeRef: Qualified;
+  TypeDef: Qualified;
+  Interface: Qualified;
+  InterfaceRef: Qualified;
+}>;
+
+/**
+ * A resolved Wit is a Wit with all the types and interfaces resolved to their
+ * actual type definitions.
+ */
+export type ResolvedWit = Wit<{
+  TypeRef: Resolved<TypeDef<ResolvedWit>>;
+  InterfaceRef: Resolved<Interface<ResolvedWit>>;
+}>;
 
 export const test: Wit = {
   name: "foo",
   version: "0.0.1",
   packages: [],
-  interfaces: [],
+  interfaces: [
+    {
+      name: "foo",
+      functions: [],
+      typeDefs: [
+        {
+          kind: "record",
+          name: "bar",
+          fields: [
+            {
+              name: "message",
+              type: "string",
+            },
+          ],
+        },
+      ],
+    },
+  ],
   uses: [
     {
       path: "other:math/operations.add",
